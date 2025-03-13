ffc82ec2de56eafc2ddfe1217733caa2b3c0cbe6
MK4 <mithunkarthik123@gmail.com>
2025-03-13 17:47:46 +0530

    up1

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -1,17 +1,6 @@
 import "./globals.css";
 import Providers from "./provider";
 
-export const metadata = {
-  title: "Wootz 2025",
-  description: "Your website description here",
-};
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 0.9,
-  maximumScale : 0.9,
-  userScalable: false,
-};
 
 export default function RootLayout({ children }) {
   return (
