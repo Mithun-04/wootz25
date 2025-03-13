b0f0e615a66dca62113b5ae4ac1b90acee8c2e6c
MK4 <mithunkarthik123@gmail.com>
2025-03-13 16:19:12 +0530

    scale-3

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { Geist } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
@@ -7,21 +7,15 @@ import { store, persistor } from "@/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./globals.css";
 
+export const metadata = {
+  title: "Wootz 2025",
+  description: "Your website description here",
+  viewport: "width=device-width, initial-scale=0.8",
+};
+
 export default function RootLayout({ children }) {
-
-  useEffect(() => {
-    const metaViewport = document.querySelector("meta[name=viewport]");
-    if (metaViewport) {
-      metaViewport.setAttribute("content", "width=device-width, initial-scale=0.8");
-    }
-  }, []);
-
   return (
     <html lang="en">
-      <head>
-        <title>Wootz 2025</title>
-        <link rel="icon" type="image/png" href="/hero-img3.png" />
-      </head>
       <body>
         <Provider store={store}>
           <PersistGate loading={<></>} persistor={persistor}>
